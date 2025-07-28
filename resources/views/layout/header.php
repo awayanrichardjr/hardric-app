20e5914c374de6e4ac94b7c4260169deede3ce44
Richard Awayan <awayanrichardjr@gmail.com>
2025-07-28 17:10:15 +0800

    first commit

---
+++ resources/views/layout/header.php
@@ -0,0 +1,5 @@
+<header class="header">
+    <h1>header</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut tenetur labore commodi quam, pariatur voluptate delectus itaque aliquid magni ad accusantium illo quos qui ipsa quae in voluptatum. Corporis, voluptatum.</p>
+    <button class="btn">button</button>
+</header>
---
+++ resources/views/layout/nav.php
@@ -0,0 +1,24 @@
+<nav class="navigation">
+    <ul class="nav-brand">
+        <li><a href="#">HARDRIC</a></li>
+    </ul>
+    <ul class="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Expertise</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+    <ul class="nav-menu">
+        <li>
+            <a href="#">
+                <span class="material-symbols-rounded">menu</span>
+            </a>
+        </li>
+    </ul>
+    <ul class="nav-sidebar">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Expertise</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
